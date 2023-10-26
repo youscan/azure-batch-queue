60c18b6e529ec26f8bba9b3a2cc76ec42f69f0ba
yaroslavtykhonchuk <yaroslav.tykhonchuk@youscan.io>
2023-10-26 15:33:35 +0300

    Tidy up

--- AzureBatchQueue.Tests/Samples/AzureStorageQueueTestFixture.cs
+++ AzureBatchQueue.Tests/Samples/AzureStorageQueueTestFixture.cs
@@ -7,17 +7,12 @@ namespace AzureBatchQueue.Tests.Samples;
 [TestFixture]
 public class AzureStorageQueueTestFixture
 {
-    private string connectionString;
-    private string queueName;
     private QueueClient queue;
 
     [OneTimeSetUp]
     public void OneTimeSetUp()
     {
-        connectionString = "DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;";
-        queueName = "hello-world-queue";
-        
-        queue = new QueueClient(connectionString, queueName);
+        queue = new QueueClient("UseDevelopmentStorage=true", "hello-world-queue");
     }
 
     [SetUp]
