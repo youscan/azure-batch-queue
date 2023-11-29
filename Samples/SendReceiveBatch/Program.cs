0fffb0053e6a54c916a7417201742c8fe095edae
yaroslavtykhonchuk <yaroslav.tykhonchuk@youscan.io>
2023-11-29 14:10:54 +0200

    Remove legacy code

--- Samples/SendReceiveBatch/Program.cs
+++ Samples/SendReceiveBatch/Program.cs
@@ -15,14 +15,9 @@ var serilogLogger = new LoggerConfiguration()
 
 var microsoftLogger = new SerilogLoggerFactory(serilogLogger).CreateLogger<BatchQueue<string>>();
 
-Console.WriteLine("Compress batches using GZip? Write Yes/No and press Enter.");
-var response = Console.ReadLine();
-var serializer = GetSerializer<string>(response?.ToLower() == "yes");
-Console.WriteLine($"Will use {serializer.GetType()} serializer.");
-
 var sender = Task.Run(async () =>
 {
-    var sender = new Sender(queueName, microsoftLogger, serializer);
+    var sender = new Sender(queueName, microsoftLogger);
     await sender.Init();
     await sender.SendMessages();
 });
@@ -35,5 +30,3 @@ var receiver = Task.Run(async () =>
 });
 
 await Task.WhenAll(sender, receiver);
-
-static IMessageBatchSerializer<T> GetSerializer<T>(bool compressed) => compressed ? new GZipCompressedSerializer<T>() : new JsonSerializer<T>();
--- Samples/SendReceiveBatch/Receiver.cs
+++ Samples/SendReceiveBatch/Receiver.cs
@@ -28,24 +28,29 @@ public class Receiver
         {
             var batchItems = await batchQueue.Receive();
 
-            Log($"Received batch with {batchItems.Length} items.");
-
-            foreach (var batchItem in batchItems)
-            {
-                if (DoNotComplete.Contains(batchItem.Item))
-                {
-                    Log($"Will not complete {batchItem.Item}");
-                    continue;
-                }
-
-                Log($"Completed {batchItem.Item}");
-                batchItem.Complete();
-            }
+            if (batchItems.Length > 0)
+                ProcessBatch(batchItems);
 
             Sleep(TimeSpan.FromSeconds(2));
         }
 
         Log("Finish receiving items.");
+    }
+
+    void ProcessBatch(BatchItem<string>[] batchItems)
+    {
+        Log($"Received batch with {batchItems.Length} items.");
+        foreach (var batchItem in batchItems)
+        {
+            if (DoNotComplete.Contains(batchItem.Item))
+            {
+                Log($"Will not complete {batchItem.Item}");
+                continue;
+            }
+
+            Log($"Completed {batchItem.Item}");
+            batchItem.Complete();
+        }
     }
 
     static void Sleep(TimeSpan sleep)
